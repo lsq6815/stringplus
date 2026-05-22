820993cb196e42ef5b2cc238d1328e22c3ead697
lsq6815 <lsq6815@gmail.com>
2026-05-23 01:19:53 +0800

    docs(stringplus.h): 修复 Doxygen 参数名和命令

--- include/stringplus.h
+++ include/stringplus.h
@@ -11,25 +11,25 @@
 #define NOP ((size_t)-1)
 
 /**
- * @brief 查找 @ch 在 @str 中第一次出现的位置
+ * @brief 查找 chr 在 str 中第一次出现的位置
  *
  * @param str : C 风格字符串
- * @param ch  : 要查找的字符
- * @return 如果 @str 中没有 @ch 返回 NOP，否则返回下标
+ * @param chr : 要查找的字符
+ * @return 如果 str 中没有 chr 返回 NOP，否则返回下标
  */
 size_t indexof(const char *str, int chr);
 
 /**
- * @brief 查找 @charset 中任意字符在 @str 中第一次出现的位置
+ * @brief 查找 charset 中任意字符在 str 中第一次出现的位置
  *
  * @param str     : C 风格字符串
  * @param charset : 要查找的字符集合
- * @return 如果 @str 中没有任何匹配字符返回 NOP，否则返回下标
+ * @return 如果 str 中没有任何匹配字符返回 NOP，否则返回下标
  */
 size_t indexamong(const char *str, const char *charset);
 
 /**
- * @brief 使用 @delims 中的分隔符分割 @str
+ * @brief 使用 delims 中的分隔符分割 str
  *
  * @param str    : 待分割的字符串
  * @param delims : 分隔符集合
@@ -38,7 +38,7 @@ size_t indexamong(const char *str, const char *charset);
 pccary split(const char *str, const char *delims);
 
 /**
- * @brief 使用 @jch 将 @strset 中的字符串连接成单个字符串
+ * @brief 使用 jch 将 strset 中的字符串连接成单个字符串
  *
  * @param strset : 字符串集合
  * @param jch    : 用于连接字符串的字符
