7b69e1e8747e1bf6aa1ea1c0ac6431cc8e911176
lsq6815 <lsq6815@gmail.com>
2026-05-23 01:19:08 +0800

    docs(ccary.h): 修复 Doxygen 参数名和命令

--- include/ccary.h
+++ include/ccary.h
@@ -22,15 +22,15 @@ typedef void (*ccary_foreach_func)(void *);
 pccary ccary_init(void);
 
 /**
- * @brief 销毁 @pcca 指向的 ccary，释放所有空间
+ * @brief 销毁 pcca 指向的 ccary，释放所有空间
  *
  * @param pcca : 指向 ccary 的指针
  */
 void ccary_destroy(pccary pcca);
 
 /**
- * @brief 遍历 @pcca 中的每个字符串并调用 @func
- * 注意：在 @func 中调用 free() 会导致内存泄漏！
+ * @brief 遍历 pcca 中的每个字符串并调用 func
+ * 注意：在 func 中调用 free() 会导致内存泄漏！
  *
  * @param pcca : 指向 ccary 的指针
  * @param func : 处理字符串的函数
@@ -38,7 +38,7 @@ void ccary_destroy(pccary pcca);
 void ccary_foreach(pccary pcca, ccary_foreach_func func);
 
 /**
- * @brief 清理 @pcca 指向的所有空间，并将 size 置为 0
+ * @brief 清理 pcca 指向的所有空间，并将 size 置为 0
  *
  * @param pcca : 指向 ccary 的指针
  */
@@ -53,7 +53,7 @@ void ccary_clean(pccary pcca);
 size_t ccary_size(pccary pcca);
 
 /**
- * @brief 将 @str 追加到 @pcca
+ * @brief 将 str 追加到 pcca
  *
  * @param pcca : 指向 ccary 的指针
  * @param str  : C 风格字符串
@@ -61,7 +61,7 @@ size_t ccary_size(pccary pcca);
 void ccary_append(pccary pcca, const char *str);
 
 /**
- * @brief 显示 @pcca 的内容
+ * @brief 显示 pcca 的内容
  *
  * @param pcca : 指向 ccary 的指针
  */
